779d2e01457c1d917e41e66b0278c72cc2732652
aziz herch <aziz.herch@ensi-uma.tn>
2019-06-27 14:32:21 +0200

    m

--- src/main/scala/GeneratingService.scala
+++ src/main/scala/GeneratingService.scala
@@ -1,6 +1,7 @@
 import java.util.{Date, Random}
 import java.text.DateFormat
 import java.text.SimpleDateFormat
+import java.io.{File, FileNotFoundException, PrintWriter}
 
 
 
@@ -8,6 +9,7 @@ object GeneratingService {
   def main(args: scala.Array[String]): Unit = {
 
     println(generateRandomDate(13800))
+    writeRefFile("/home/herch/Documents/work/WorkSpace/phenix-challenge/src/resources/output/new3dssdssss3g2g.txt")
 
   }
 
@@ -36,19 +38,67 @@ object GeneratingService {
 
 //generate random Id magasin.
   def generateRandomIdMagasin: String = {
-    generateRandomString(8) + "-" + generateRandomString(4) + "-" + generateRandomString(4) + "-" + generateRandomString(4) + "-" + generateRandomString(12)
+    generateRandomString(8) + "-" + generateRandomString(5) + "-" + generateRandomString(4) + "-" + generateRandomString(4) + "-" + generateRandomString(12)
   }
 
 
 
   def generateRandomDate(n: Int): String = {
-    val simple = new SimpleDateFormat("yyyyMMdd HHmmssSSSZ")
+    val simple = new SimpleDateFormat("yyyyMMdd'T'HHmmssSSSZ")
     val result = new Date(System.currentTimeMillis+n*5445)
     simple.format(result)
   }
 
 
+  import java.io.FileNotFoundException
+  import java.io.PrintWriter
+  import java.util.Random
 
+  @throws[FileNotFoundException]
+  def generateTransactionsFile(path: String): Unit = {
+    val writer = new PrintWriter(new File(path))
+    var i = 0
+    while ( {
+      i < 100000
+    }) {
+      val rand = new Random
+      val transId = Math.abs(rand.nextInt(100))
+      val datetime = generateRandomDate(transId)
+      val magasinId = generateRandomIdMagasin
+      val produitId = Math.abs(rand.nextInt(100))
+      val qte = Math.abs(rand.nextInt(100))
+      writer.write(transId + "|" + datetime + "|" + magasinId + "|" + produitId + "|" + qte + "\n")
+
+      {
+        i += 1; i - 1
+      }
+    }
+    writer.close()
+  }
+
+
+  import java.io.FileNotFoundException
+  import java.io.PrintWriter
+  import java.util.Random
+
+  @throws[FileNotFoundException]
+  def writeRefFile(path: String): Unit = {
+    val writer = new PrintWriter(new File(path))
+    var i = 0
+    while ( {
+      i < 100000
+    }) {
+      val r = new Random
+      val produit = Math.abs(r.nextInt(100))
+      val prix = 00.01+ r.nextFloat * 100 + ""
+      writer.write(produit + "|" + prix.substring(0, 5) + "\n")
+
+      {
+        i += 1; i - 1
+      }
+    }
+    writer.close()
+  }
 
 
 
