d733dfa71932ee93b3dfdeb2d7f77e73aa804fc9
aziz herch <aziz.herch@ensi-uma.tn>
2019-06-28 15:34:40 +0200

    m

--- src/test/scala/ProcessingData/FilesUtilsTest.scala
+++ src/test/scala/ProcessingData/FilesUtilsTest.scala
@@ -20,6 +20,7 @@ class FilesUtilsTest extends FunSuite {
     })
   }
 
+  // teste la fonction export qui exporte un fichier
   test("test export") {
     val list: List[(String, Any)] = List(("v1", "v2"), ("v3", "v4"), ("v5", "v6"))
     export(list, "./src/resources/output/testExport")
@@ -29,7 +30,7 @@ class FilesUtilsTest extends FunSuite {
   }
 
 
-  //test la fonction readStream
+  //test la fonction concatFiles akka
   test("test concatFilesAkka") {
 
     implicit val system = ActorSystem("Sys")
